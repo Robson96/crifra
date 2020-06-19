68ff3259feecfb8139fd6f28597a728bf624d2e5
Robson <robson.magno2018@gmail.com>
2020-06-19 07:59:52 -0300

    teste

--- src/main/java/challenge/CriptografiaCesariana.java
+++ src/main/java/challenge/CriptografiaCesariana.java
@@ -4,12 +4,13 @@ public class CriptografiaCesariana implements Criptografia {
 
     @Override
     public String criptografar(String texto) {
+        //se for nulo ele ja lançca a NUllPionter
         if (texto.isEmpty())
             throw new IllegalArgumentException();
 
         StringBuilder builder = new StringBuilder();
 
-        for (Character character: texto.toCharArray()) {
+        for (Character character: texto.toLowerCase().toCharArray()) {
             if (Character.isAlphabetic(character)) {
                 builder.append((char) (((int)character) + 3));
             } else {
@@ -22,12 +23,13 @@ public class CriptografiaCesariana implements Criptografia {
 
     @Override
     public String descriptografar(String texto) {
+        //se for nulo ele ja lançca a NUllPionter
         if (texto.isEmpty())
             throw new IllegalArgumentException();
 
         StringBuilder builder = new StringBuilder();
 
-        for (Character character: texto.toCharArray()) {
+        for (Character character: texto.toLowerCase().toCharArray()) {
             if (Character.isAlphabetic(character)) {
                 builder.append((char) (((int)character) - 3));
             } else {
